35173ad19cb98815d50198f5ab298cddfcfd54f7
Mikerophonic <micgcon@gmail.com>
2023-06-15 20:08:37 -0700

    finish game

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const choice = options[Math.floor(Math.random() * options.length)]
-    console.log(choice)
+    const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
@@ -32,9 +31,49 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getPlayerChoice() {
+    let validatedInput = false;
+    while(validatedInput === false){
+        const choice = prompt("Rock Paper or Scissors");
+        if (choice === null) {
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("----------");
+        if (checkWinner(playerSelection, computerSelection) === "Player") {
+            scorePlayer++;
+        } else if (checkWinner(playerSelection, computerSelection) === "Computer"){
+            scoreComputer++
+        }
+    }
+    console.log("Game Over");
+    if (scorePlayer > scoreComputer) {
+        console.log("Player wins!");
+    } else if (scorePlayer < scoreComputer) {
+        console.log("Computer wins");
+    } else {
+        console.log("We have a tie!");
+    }
+
+    }
+
+
+game();
 
 /* function getComputerChoice() {
     let computerChoice; 
